beef7a889dfd2ded1ecfbb184b2a0c3cd7e13ca0
zacmonroe <zmonroe0820@gmail.com>
2023-12-03 21:45:44 -0600

    Initial Commit

---
+++ appetizers/appetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizersApp.swift
+//  appetizers
+//
+//  Created by Zac Milano on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct appetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
