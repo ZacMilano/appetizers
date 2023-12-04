7ae6a49b2aaadc7f88d59d9e3b6afecf61d087f4
zacmonroe <zmonroe0820@gmail.com>
2023-12-03 22:17:24 -0600

    feat(data): add data type, mock data

---
+++ appetizers/Model/Appetizer.swift
@@ -0,0 +1,38 @@
+//
+//  Appetizer.swift
+//  appetizers
+//
+//  Created by Zac Milano on 12/3/23.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(
+        id: 0001,
+        name: "Test Appetizer",
+        description: "This is the description for my appetizer. It's delicious.",
+        price: 9.99,
+        imageURL: "",
+        calories: 550,
+        protein: 32,
+        carbs: 40
+    )
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
--- appetizers/appetizersApp.swift
+++ appetizers/appetizersApp.swift
@@ -15,3 +15,22 @@ struct appetizersApp: App {
         }
     }
 }
+
+
+/*
+{
+    "request": [
+        {
+            "description": "This perfectly thin cut just melts in your mouth.",
+            "protein": 14,
+            "id": 1,
+            "imageURL": "https://seanallen-course-backend.herokuapp.com/images/appetizers/asian-flank-steak.jpg",
+            "calories": 300,
+            "carbs": 0,
+            "price": 8.99,
+            "name": "Asian Flank Steak"
+        }
+    ]
+}
+    """
+ */
