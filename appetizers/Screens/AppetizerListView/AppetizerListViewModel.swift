6738f827ae55bec1bc84cb9b2578a52c033c79ff
zacmonroe <zmonroe0820@gmail.com>
2023-12-04 21:37:47 -0600

    feat(error handling): alerts for errors

--- appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -9,17 +9,31 @@ import SwiftUI
 
 final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
             // Network calls are on the background thread; this is causing a UI
             // update, so we put it on the main thread
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                        
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
---
+++ appetizers/Utils/Alert.swift
@@ -0,0 +1,39 @@
+//
+//  Alert.swift
+//  appetizers
+//
+//  Created by Zac Milano on 12/4/23.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidData = AlertItem(
+        title: Text("Server Error"),
+        message: Text("The data received from the server was invalid. Please contact support."),
+        dismissButton: .default(Text("OK"))
+    )
+    static let invalidResponse = AlertItem(
+        title: Text("Server Error"),
+        message: Text("Invalid response from the server. Please try again later or contact support."),
+        dismissButton: .default(Text("OK"))
+    )
+    static let invalidURL = AlertItem(
+        title: Text("Server Error"),
+        message: Text("There was an issue connecting to the server. If this perissts, please contact support."),
+        dismissButton: .default(Text("OK"))
+    )
+    static let unableToComplete = AlertItem(
+        title: Text("Server Error"),
+        message: Text("Unable to complete your request at this time. Please check your internet connection."),
+        dismissButton: .default(Text("OK"))
+    )
+}
+
