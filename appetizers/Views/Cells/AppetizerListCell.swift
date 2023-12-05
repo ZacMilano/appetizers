a91a5d7f87472d8d393cd6d2e228c28721737307
zacmonroe <zmonroe0820@gmail.com>
2023-12-04 22:16:17 -0600

    feat(images): add image loading :D

--- appetizers/Views/Cells/AppetizerListCell.swift
+++ appetizers/Views/Cells/AppetizerListCell.swift
@@ -12,8 +12,7 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            Image("asian-flank-steak")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120)
                 .cornerRadius(8)
