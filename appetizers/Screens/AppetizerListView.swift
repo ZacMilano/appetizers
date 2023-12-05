3dde971129d603b7cc0317f809228d7059e3de9d
zacmonroe <zmonroe0820@gmail.com>
2023-12-04 21:05:38 -0600

    feat(data): use the data from HTTP

--- appetizers/Screens/AppetizerListView.swift
+++ appetizers/Screens/AppetizerListView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    @State private var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍗 Appetizers")
+        }
+        .onAppear {
+            getAppetizers()
+        }
+    }
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            // Network calls are on the background thread; this is causing a UI
+            // update, so we put it on the main thread
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
